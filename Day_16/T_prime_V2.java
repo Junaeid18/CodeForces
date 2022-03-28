b918e376e94eef850519c45c41d7f91a85e7cf1c
Junaeid18 <mdjunaeid98@gmail.com>
2022-03-28 22:35:26 +0600

    T-prime problem with version2 also

---
+++ Day_16/T_prime_V2.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class T_prime_V2 {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        ArrayList<Long> list = new ArrayList<>();
+        int limit = (int)1e6;
+        int[] ar = new int[limit];
+        list.add((long)4);
+        for (int i = 3; i <= limit; i+=2) {
+            if(ar[i] == 0){
+                list.add((long)i*i);
+                for (int j = i; j < ar.length; j+=i) {
+                    ar[j] = 1;
+                }
+            }
+        }
+        int count = 0, testCase = s.nextInt();
+        while(count < testCase){
+            long test = s.nextLong();
+            long test1 = (int)Math.sqrt(test);
+            if(test == 1){
+                System.out.println("NO");
+            }else if(test1* test1 == test){
+                if(list.contains(test)){
+                    System.out.println("YES");
+                }else{
+                    System.out.println("NO");    
+                }
+            }else{
+                System.out.println("NO");
+            }
+            ++count;
+        }
+    }
+}
+
+
+/* 
+
+            if(test < (long)1e6){
+                test = (int)test;
+                if(test == 1){
+                    System.out.println("NO");
+                }else if(test1* test1 == test){
+                    if(list.contains((int)test)){
+                        System.out.println("YES");
+                    }
+                }else{
+                    System.out.println("NO");
+                }
+            }else{
+                if(test1* test1 == test){
+                    if(list.contains((int)test)){
+                        System.out.println("YES");
+                }
+            }
+
+
+*/
--- Day_16/T_primes.java
+++ Day_16/T_primes.java
@@ -1,10 +1,42 @@
 import java.lang.Math;
+import java.util.ArrayList;
 import java.util.Scanner;
 public class T_primes {
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
-        int count = 0,divisor, num = s.nextInt();
-        long testNum;
+        ArrayList<Double> list = new ArrayList<Double>();
+        boolean b; 
+        double limit = 1000000;
+        double testNum;
+        for (double i = 2; i <= limit; i++) {
+            b = false;
+            for (double j = 2; j <= Math.sqrt(i); j++) {
+                if(i % j == 0){
+                    b = true;
+                } 
+            }
+            if(b == false){
+                System.out.println(i);
+                list.add(i);  
+            } 
+        }
+        int count = 0 , testCase = s.nextInt(); 
+        while(count < testCase){
+            testNum = s.nextDouble();
+            testNum = Math.sqrt(testNum);
+            System.out.println(testNum);
+            if(list.contains(testNum)){
+                System.out.println("YES");
+            }else{
+                System.out.println("NO");
+            }
+            ++count;
+        }
+    }    
+}
+
+
+/* 
         while(count < num){
             testNum = s.nextLong();
             divisor = 0;
@@ -25,5 +57,5 @@ public class T_primes {
             }
             ++count;
         }
-    }    
-}
+
+*/
