28a781247a1832f09017a68f9c654f92d6f9a825
Junaeid18 <mdjunaeid98@gmail.com>
2022-03-30 01:23:36 +0600

    Day17 added to the repo

---
+++ Day_17/Learn_From_Math.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Learn_From_Math {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int limit = 1e6+1;
+        int[] ar = new int[limit];
+
+    }    
+}
