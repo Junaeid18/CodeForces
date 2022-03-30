2859e4682c1bce00bfa7afdc0dae0bd9b970a29a
Junaeid18 <mdjunaeid98@gmail.com>
2022-03-31 01:47:01 +0600

    Problem 472A Learn from math solved

--- Day_17/Learn_From_Math.java
+++ Day_17/Learn_From_Math.java
@@ -4,6 +4,26 @@ public class Learn_From_Math {
         Scanner s = new Scanner(System.in);
         int limit = (int)1e6+1;
         int[] ar = new int[limit];
-
+        ar[2] = 1;
+        for (int i = 3; i < limit; i+=2) {
+            ar[i] = 1;
+        }
+        for (int i = 3; i < limit; i+=2) {
+            if(ar[i] == 1){
+                for (int j = 2 * i; j < limit; j +=i) {
+                    ar[j] = 0;
+                }
+            }
+        }
+        int num = s.nextInt(),temp;
+        for (int i = 4; i <= num; i++) {
+            if(ar[i]==0){
+                temp = num - i;
+                if(ar[temp] == 0){
+                    System.out.println(i+" "+temp);
+                    break;
+                }
+            }
+        }
     }    
 }
