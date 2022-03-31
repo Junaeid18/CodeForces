bf6e3d8362e788f4e9780d08e0af517c51357bf7
Junaeid18 <mdjunaeid98@gmail.com>
2022-03-31 23:56:54 +0600

    Balanced Array problem solved with little modification,also help from tutorial

--- Day_17/Balanced_Array.java
+++ Day_17/Balanced_Array.java
@@ -3,28 +3,20 @@ public class Balanced_Array {
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
         int count = 0, test = s.nextInt();
-        long num,sum1,sum2;
+        long num;
         while(count < test){
             num = s.nextLong();
             if(num %2 == 0){
-                if((num/2)%2 == 0){
-                    sum1 = sum2 = 0;
-                    String out = "";
-                    for (long i = 2; i <= num; i += 2) {
-                        sum1 += i;
-                        out += i+" ";
-                    }
-                    if((num/2) < 4 ){
-                        out +="1 5";
-                    }else{
-                        for (long j = 1; j < num-1; j+=2) {
-                            sum2 += j;
-                            out +=j+" ";
-                        }
-                        long l = sum1-sum2; out += l;
-                    }
+                num /= 2;
+                if(num%2 == 0){
                     System.out.println("YES");
-                    System.out.println(out);
+                    for (long i = 1; i <= num; i++) {
+                        System.out.print( 2 * i+" ");
+                    }
+                    for (long i = 1; i < num; i++) {
+                        System.out.print((2 * i ) - 1+" ");
+                    }
+                    System.out.println((3 * num) - 1);
                 }else{
                     System.out.println("NO");    
                 }
