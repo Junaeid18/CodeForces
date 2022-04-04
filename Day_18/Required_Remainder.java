abe58db866629c5dc8b76380caf4d8cba44a67f6
Junaeid18 <mdjunaeid98@gmail.com>
2022-04-05 01:14:57 +0600

    Required remainder solved

--- Day_18/Required_Remainder.java
+++ Day_18/Required_Remainder.java
@@ -2,12 +2,16 @@ import java.util.Scanner;
 public class Required_Remainder {
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
-        int count = 0,x,y,n, testCase = s.nextInt();
+        int count = 0,x,y,n,ans1,num,step,fAns, testCase = s.nextInt();
         while(count < testCase){
             x = s.nextInt(); y = s.nextInt(); n = s.nextInt();
-            if(x > n){System.out.println(0);}
+            ans1 = num = step = fAns = 0;
+            ans1 = n % x;
+            if(ans1 == y){ System.out.println(n);}
             else{
-                
+                if(ans1 > y){   num = ans1 - y; fAns = n - num;}
+                else{  step = (n / x) - 1; fAns = (step * x) + y; }
+                System.out.println(fAns);    
             }
             count++;
         }
