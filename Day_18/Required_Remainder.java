6e470530a19dad92ef3c91fca923dcf6326d9bb4
Junaeid18 <mdjunaeid98@gmail.com>
2022-04-04 23:53:27 +0600

    Problem 1374A trying

---
+++ Day_18/Required_Remainder.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Required_Remainder {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int count = 0,x,y,n, testCase = s.nextInt();
+        while(count < testCase){
+            x = s.nextInt(); y = s.nextInt(); n = s.nextInt();
+            if(x > n){System.out.println(0);}
+            else{
+                
+            }
+            count++;
+        }
+    }
+}
