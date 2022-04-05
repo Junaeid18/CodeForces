4b03ead72a630dc00a40f7711ebf06d426a3e254
Junaeid18 <mdjunaeid98@gmail.com>
2022-04-06 05:04:43 +0600

    Problem 1376B Even Array tried, not solved yet

---
+++ Day_18/Even_Array.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Even_Array {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int count = 0,arrSize,inCount,num,i,j,evenC,oddC,step, testCase = s.nextInt();
+        while(count < testCase){
+            arrSize = s.nextInt(); inCount = step = evenC = oddC = 0; i = 0 ; j = 1; 
+            while(inCount < arrSize){
+                num = s.nextInt();  
+                if(num % 2 == 0){   evenC++;
+                    if(inCount% 2 != 0){
+                        if(Math.max(inCount-i, i-inCount) > 0){ step++; i += 2;}
+                    }
+                }else{  oddC++;
+                    if(inCount%2 == 0){     
+                        if(Math.max(inCount-j, j-inCount) > 0){ step++; j += 2;}
+                    }
+                }
+                inCount++;
+            }
+            if(arrSize%2==0){if((j-i == 1) && (evenC%2 == 0)){System.out.println(step/2);}  else{System.out.println(-1);}}
+            else{
+                if(((j-i == 1) || (i-j == 1 )) && (oddC%2 != 0) ){
+                    System.out.println(step/2);
+                }else{  System.out.println(-1);}
+            }
+            count++;
+        }
+    }
+}
+
+
+//step += Math.max(inCount-i, i-inCount); i += 2;
+//step += Math.max(inCount-j, j-inCount); j += 2;
+//System.out.println("Step : "+step);
