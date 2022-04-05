6891f7ed137fadcdc3105f3a9fc685122ed8252b
Junaeid18 <mdjunaeid98@gmail.com>
2022-04-05 19:59:39 +0600

    Soft Drinking problem initialized

---
+++ Day_18/Soft_Drinking.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Soft_Drinking {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int count = 0, n = s.nextInt(), k = s.nextInt(),l = s.nextInt(),c = s.nextInt(),d = s.nextInt();
+        int p = s.nextInt(), nl = s.nextInt(),np = s.nextInt();
+        int OneToast = n * nl, noOfToast =  (k * l) / OneToast;
+        int totalLime = c * d; 
+    }    
+}
