4a6e93b9f03bf1077e5131edbe8638a3f2ea5ab9
Junaeid18 <mdjunaeid98@gmail.com>
2022-04-06 03:08:00 +0600

    Problem 151A Soft Drinking solved

--- Day_18/Soft_Drinking.java
+++ Day_18/Soft_Drinking.java
@@ -2,9 +2,42 @@ import java.util.Scanner;
 public class Soft_Drinking {
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
-        int count = 0, n = s.nextInt(), k = s.nextInt(),l = s.nextInt(),c = s.nextInt(),d = s.nextInt();
+        int n = s.nextInt(), k = s.nextInt(),l = s.nextInt(),c = s.nextInt(),d = s.nextInt();
         int p = s.nextInt(), nl = s.nextInt(),np = s.nextInt();
         int OneToast = n * nl, noOfToast =  (k * l) / OneToast;
-        int totalLime = c * d; 
+        if(c * d < noOfToast * n){
+            while(!(c * d >= noOfToast * n)){
+                if(noOfToast > 0){  --noOfToast;}
+            }
+        }
+        if(n * np * noOfToast > p){
+            while(!(n * np * noOfToast <= p)){
+                if(noOfToast > 0){  --noOfToast;}
+            }
+        }
+        System.out.println(noOfToast); 
     }    
 }
+
+
+//int totalLime = c * d;
+/* 
+
+if(c * d >= noOfToast * n){
+    if(n * np * noOfToast <= p){
+        System.out.println(noOfToast);
+    }else{
+        while(!(n * np * noOfToast <= p)){
+                    if(noOfToast > 0){  --noOfToast;}
+                }
+                System.out.println(noOfToast);    
+            }
+        }else{
+            while(!(c * d >= noOfToast * n)){
+                if(noOfToast > 0){  --noOfToast;}
+            }
+        }
+
+
+
+*/
