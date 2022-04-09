6f9ddfc19e15c8f6cd8782b19fa1149ed015edd2
Junaeid18 <mdjunaeid98@gmail.com>
2022-04-09 16:23:31 +0600

    Problem 579A Raising bacteria Solved using bitmask

---
+++ Day_18/Raising_BacteriaV2.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Raising_BacteriaV2 {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        System.out.println(Integer.bitCount(n));
+    }    
+}
