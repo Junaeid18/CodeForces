542074fe2bfe9e24aae604368a70f2c3d987b096
Junaeid18 <mdjunaeid98@gmail.com>
2022-04-12 03:48:38 +0600

    Problem 489C not solved

--- Day_19/GivenLengthAndSumOfDigit.java
+++ Day_19/GivenLengthAndSumOfDigit.java
@@ -1,10 +1,16 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 public class GivenLengthAndSumOfDigit {
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
         int m = s.nextInt(), sum = s.nextInt(), rem = sum, len = m, count = 0;
-        String res = "";    StringBuilder revRes = new StringBuilder();
-        if(m < sum){
+        String res = "",res2 = "";    StringBuilder revRes = new StringBuilder();
+        if(sum == 0){
+            System.out.println(-1 +" "+ -1);
+        }else if(m < sum){
+            list.clear();
             while(count < m){
                 if(rem > 9){
                     res += "9"; rem -= 9;   --len;
@@ -15,10 +21,27 @@ public class GivenLengthAndSumOfDigit {
                 }
                 count++;
             }
+            for (int i = 0; i < m; i++) {
+                list.add(Character.getNumericValue(res.charAt(i)));
+            }
+            Collections.sort(list,Collections.reverseOrder());
+            for (int i = 0; i < m; i++) {
+                res2 += list.get(i);
+            }
             revRes.append(res).reverse();
-            System.out.println(revRes+" "+res);
+            System.out.println(revRes+" "+res2);
         }else{
-            System.out.println(-1 +" "+ -1);
+            while(count < m){
+                if(rem > 9){
+                    res += "9"; rem -= 9;   --len;
+                }else{
+                    if(len > 0){
+                        int nextDigit = rem; res += String.valueOf(nextDigit);  rem -= nextDigit;  --len;
+                    }
+                }
+                count++;
+            }
+            System.out.println(res+" "+res);
         }
     }
 }
