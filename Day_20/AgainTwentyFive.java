e8ec9e0ac452c861f4037513b5ba8571ff595dc6
Junaeid18 <mdjunaeid98@gmail.com>
2022-04-18 00:28:54 +0600

    Problem 630A Again Twenty Five solved, Tricky easy

---
+++ Day_20/AgainTwentyFive.java
@@ -0,0 +1,16 @@
+/*
+    Junaeid As Jknight
+    Somoy paile dekha koiro
+    alap-adda-gopposob hobe
+    cha or coffee er sathe.
+------<<<<<<*>>>>>>>>-----
+*/
+import java.util.Scanner;
+public class AgainTwentyFive {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        String num = s.nextLine();
+        System.out.println(25);
+        s.close();
+    }
+}
