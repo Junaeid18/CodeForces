2347bede51b63b943b51294f96e8a3a63355fb7f
Junaeid18 <mdjunaeid98@gmail.com>
2022-04-23 05:05:52 +0600

    Problem 492A Vanya and cubes solved

---
+++ Day_22/VanyaAndCubes.java
@@ -0,0 +1,20 @@
+/*
+    Junaeid As Jknight
+    Somoy paile dekha koiro
+    alap-adda-gopposob hobe
+    cha or coffee er sathe.
+------<<<<<<*>>>>>>>>-----
+*/
+import java.util.Scanner;
+public class VanyaAndCubes {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt(),height=0,rem=n,lvlCube,lvl=1;   boolean run = false;
+        while(!run){
+            lvlCube = 0;    
+            for (int i = 1; i <= lvl; i++) {    lvlCube += i; }
+            if(rem >= lvlCube){rem -= lvlCube; ++lvl;}  else{run=true;}
+        }
+        System.out.println(lvl-1);
+    }
+}
