d5fe7b73e6888143cf3be1e7f7a573dfcca96a75
Junaeid18 <mdjunaeid98@gmail.com>
2022-05-03 00:34:11 +0600

    Problem 1325A Ehad and gcd solved

---
+++ Day_24/EhAbAnDgCd.java
@@ -0,0 +1,18 @@
+/*
+    Junaeid As Jknight
+    Somoy paile dekha koiro
+    alap-adda-gopposob hobe
+    cha or coffee er sathe.
+------<<<<<<*>>>>>>>>-----
+D-24
+*/
+import java.util.Scanner;
+public class EhAbAnDgCd {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int count = 0,testCase = s.nextInt(),n;
+        while(count < testCase){
+            n = s.nextInt();    System.out.println(1+" "+(n-1));    ++count;    
+        }s.close();
+    }    
+}
