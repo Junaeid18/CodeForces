109201bd67c595122a5f90ae44737a5776756900
Junaeid18 <mdjunaeid98@gmail.com>
2022-05-11 05:04:18 +0600

    Problem 1186A Vus the cossack and a Contest solved

---
+++ Day-26/VusTheCossackAndaContest.java
@@ -0,0 +1,17 @@
+/*
+    Junaeid As Jknight
+    Somoy paile dekha koiro
+    alap-adda-gopposob hobe
+    cha or coffee er sathe.
+------<<<<<<*>>>>>>>>-----
+D-26
+*/
+import java.util.Scanner;
+public class VusTheCossackAndaContest {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt(), m = s.nextInt(), k = s.nextInt();
+        if(m >= n && k >= n)System.out.println("Yes");
+        else System.out.println("No");  s.close();
+    }
+}
