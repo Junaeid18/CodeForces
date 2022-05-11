443f00199817eb334e546c666d3d17a829f0e62d
Junaeid18 <mdjunaeid98@gmail.com>
2022-05-11 18:27:35 +0600

    Problem 1372A Omkar and completion solved

---
+++ Day_27/OmkarAndCompletion.java
@@ -0,0 +1,21 @@
+/*
+    Junaeid As Jknight
+    Somoy paile dekha koiro
+    alap-adda-gopposob hobe
+    cha or coffee er sathe.
+------<<<<<<*>>>>>>>>-----
+D-27
+*/
+import java.util.Scanner;
+import java.util.Arrays;
+public class OmkarAndCompletion {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        StringBuilder out = new StringBuilder();
+        int testCase = s.nextInt(),n; 
+        while(testCase-- > 0){
+            n = s.nextInt();    while(n-- > 1){out.append("1 ");}   out.append("1"+"\n");
+        }   s.close();
+        System.out.print(out);
+    }
+}
