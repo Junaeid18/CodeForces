718f75d2b32f4d7a79bca94423977c501cdec32a
Junaeid18 <mdjunaeid98@gmail.com>
2022-05-16 16:30:53 +0600

    Jmain snippet for Java updated in vsCode

--- Random/Practice_demo.java
+++ Random/Practice_demo.java
@@ -1,23 +1,76 @@
-import java.util.Scanner;
-
-import javax.swing.text.html.FormView;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
 public class Practice_demo {
-    public static void main(String[] args){
-        Scanner s = new Scanner(System.in);
-        int a = gcd(20,40);
+    static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next(){
+            while(st==null || !st.hasMoreTokens()){
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+        long nextLong(){
+            return Long.parseLong(next());
+        }
+        double nextDouble(){
+            return Double.parseDouble(next());
+        }
+        String nextLine(){
+            String str="";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+        private final BufferedWriter bw;
 
-        System.out.println(a);
-        for (int i = 0; i < 10; i++) {
-            if(i == 4){continue;}// break will stop the loop and continue will skip that situation in the loop
-            System.out.println("Printing : "+i);    
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
         }
 
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
     }
-    public static int gcd(int a, int b){
-        if(b == 0){
-            return a;
-        }else{
-            return gcd(b,a%b);
+    public static void main(String[] args){
+        try {
+            FastReader s = new FastReader();
+            FastWriter out = new FastWriter();
+            int testCases = s.nextInt();
+
+
+
+            out.close();
+        } catch (Exception e) {
+            return;
         }
     }
 }
