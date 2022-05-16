0cb0c2cf8c8ee18ad270feea5716cdca7b58430b
Junaeid18 <mdjunaeid98@gmail.com>
2022-05-17 04:06:18 +0600

    Problem 1650A Vasya and Coins solved

---
+++ Day_29/VasyaAndCoins.java
@@ -0,0 +1,84 @@
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+public class VasyaAndCoins {
+    static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next(){
+            while(st==null || !st.hasMoreTokens()){
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+        long nextLong(){
+            return Long.parseLong(next());
+        }
+        double nextDouble(){
+            return Double.parseDouble(next());
+        }
+        String nextLine(){
+            String str="";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+    public static void main(String[] args){
+        try {
+            StringBuilder output = new StringBuilder();
+            FastReader s = new FastReader();
+            FastWriter out = new FastWriter();
+            int testCases = s.nextInt();
+            long a,b,result;
+            while(testCases-- > 0){
+                a = s.nextLong();   b = s.nextLong();
+                if(a > 0 && b > 0)result = a+b*2+1;
+                else if(a > 0 && b == 0)result = a+1;
+                else result = 1;
+                output.append(result+"\n");
+            }
+            out.print(output);
+            out.close();
+        } catch (Exception e) {
+            return;
+        }
+    }
+}
+
