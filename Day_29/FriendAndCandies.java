d98c3a369e8a04ddff8325609c0e2759454ddd22
Junaeid18 <mdjunaeid98@gmail.com>
2022-05-17 12:18:14 +0600

    Problem 1538B Friends and candies solved

--- Day_29/FriendAndCandies.java
+++ Day_29/FriendAndCandies.java
@@ -4,7 +4,7 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.IOException;
-public class VasyaAndCoins {
+public class FriendAndCandies {
     static class FastReader{
         BufferedReader br;
         StringTokenizer st;
@@ -62,25 +62,29 @@ public class VasyaAndCoins {
     }
     public static void main(String[] args){
         try {
-            //StringBuilder output = new StringBuilder();
-            //Checking another time for Execution Time
             FastReader s = new FastReader();
             FastWriter out = new FastWriter();
-            int testCases = s.nextInt();
-            long a,b,result;
+            int testCases = s.nextInt(),n;
+            long sum, ar[],result,avg;
             while(testCases-- > 0){
-                a = s.nextLong();   b = s.nextLong();
-                if(a > 0 && b > 0)result = a+b*2+1;
-                else if(a > 0 && b == 0)result = a+1;
-                else result = 1;
-                //output.append(result+"\n");
+                n = s.nextInt(); ar = new long[n];  sum = result = avg = 0;
+                for (int i = 0; i < n; i++) {
+                    ar[i] = s.nextLong();   sum += ar[i];
+                }
+                if(n < 2)result = 0;
+                else{
+                    if(sum%n == 0){
+                        avg = sum/n;
+                        for (int i = 0; i < n; i++) {
+                            if(ar[i] > avg)++result;
+                        }
+                    }else result = -1;
+                }
                 out.println(result);
             }
-            //out.print(output);
             out.close();
         } catch (Exception e) {
             return;
         }
     }
 }
-
