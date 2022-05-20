468c2a62c225a54c60d31161415ce01499fa8b58
Junaeid18 <mdjunaeid98@gmail.com>
2022-05-20 15:10:11 +0600

    Problem 1618A Polycarp And Sums of Subsequences solved

---
+++ Day_31/PolycarpAndSumsofSubsequences.java
@@ -0,0 +1,95 @@
+/*
+    Junaeid As Jknight
+    Somoy paile dekha koiro
+    alap-adda-gopposob hobe
+    cha or coffee er sathe.
+------<<<<<<*>>>>>>>>-----
+D-31    Problem - Polycarp And Sums of Subsequences
+*/
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+public class PolycarpAndSumsofSubsequences {
+    static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next(){
+            while(st==null || !st.hasMoreTokens()){
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+        long nextLong(){
+            return Long.parseLong(next());
+        }
+        double nextDouble(){
+            return Double.parseDouble(next());
+        }
+        String nextLine(){
+            String str="";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+    public static void main(String[] args){
+        try {
+            ArrayList<Long> list = new ArrayList<>();
+            FastReader s = new FastReader();
+            FastWriter out = new FastWriter();
+            int testCases = s.nextInt(),i; 
+            long element;
+            while(testCases-- > 0){
+                i = 7;  list.clear();
+                while(i-- > 0)list.add(s.nextLong());
+                element = list.get(0) + list.get(1);    list.remove(element);
+                element = list.get(0) + list.get(2);    list.remove(element);
+                element = list.get(1) + list.get(2);    list.remove(element);
+                element = list.get(0) + list.get(1) + list.get(2);    list.remove(element);
+                for (int j = 0; j < 3; j++) {
+                    out.print(list.get(j)+" ");
+                }   out.println("");
+            }
+            out.close();
+        } catch (Exception e) {
+            return;
+        }
+    }
+}
