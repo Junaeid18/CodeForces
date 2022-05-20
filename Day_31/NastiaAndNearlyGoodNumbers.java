70a25557c66f1b3df1282dcccd322813c258c6dc
Junaeid18 <mdjunaeid98@gmail.com>
2022-05-20 14:42:49 +0600

    Problem 1521A Nastia and Nearly Good Numbers solved

---
+++ Day_31/NastiaAndNearlyGoodNumbers.java
@@ -0,0 +1,92 @@
+/*
+    Junaeid As Jknight
+    Somoy paile dekha koiro
+    alap-adda-gopposob hobe
+    cha or coffee er sathe.
+------<<<<<<*>>>>>>>>-----
+D-31    Problem - 1521A - Nastia And Nearly Good Numbers
+*/
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+public class NastiaAndNearlyGoodNumbers {
+    static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next(){
+            while(st==null || !st.hasMoreTokens()){
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+        long nextLong(){
+            return Long.parseLong(next());
+        }
+        double nextDouble(){
+            return Double.parseDouble(next());
+        }
+        String nextLine(){
+            String str="";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+    public static void main(String[] args){
+        try {
+            FastReader s = new FastReader();
+            FastWriter out = new FastWriter();
+            int testCases = s.nextInt();
+            long a,b,x,y,z;
+            while(testCases-- > 0){
+                a = s.nextLong();   b = s.nextLong();
+                x = a * b;
+                if(x == a)out.println("NO");
+                else{
+                    out.println("YES");
+                    out.println(x +" "+a+" "+(x+a));
+                }
+            }
+            out.close();
+        } catch (Exception e) {
+            return;
+        }
+    }
+}
+
