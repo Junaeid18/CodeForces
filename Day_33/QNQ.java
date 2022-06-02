d1cec21797c18454145ff345ea065f922bd2f672
Junaeid18 <mdjunaeid98@gmail.com>
2022-06-02 16:19:34 +0600

    Problem 894A QAQ solved

---
+++ Day_33/QNQ.java
@@ -0,0 +1,94 @@
+/*
+    Junaeid As Jknight
+    Somoy paile dekha koiro
+    alap-adda-gopposob hobe
+    cha or coffee er sathe.
+------<<<<<<*>>>>>>>>-----
+D-33 Problem - 894A - QNQ
+*/
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+public class QNQ {
+    static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next(){
+            while(st==null || !st.hasMoreTokens()){
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+        long nextLong(){
+            return Long.parseLong(next());
+        }
+        double nextDouble(){
+            return Double.parseDouble(next());
+        }
+        String nextLine(){
+            String str="";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+    public static void main(String[] args){
+        try {
+            FastReader s = new FastReader();
+            FastWriter out = new FastWriter();
+            int Qpos=0,result=0,Acount,size,i;
+            String input = s.nextLine();
+            size = input.length();
+            while(Qpos < (size - 2)){
+                Acount = 0;
+                while(input.charAt(Qpos) != 'Q' && (Qpos < size-1)){++Qpos;}
+                ++Qpos;    i = Qpos;
+                while(i < size){
+                    if(input.charAt(i) == 'A')++Acount;
+                    else if(input.charAt(i) == 'Q')result+=Acount;
+                    ++i;
+                }
+            }
+            out.println(result);
+            out.close();
+        } catch (Exception e) {
+            return;
+        }
+    }
+}
