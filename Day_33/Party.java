01104802cd084289c91d27c91e8e69a1a150485c
Junaeid18 <mdjunaeid98@gmail.com>
2022-06-03 18:51:10 +0600

    Problem 115A Party tried

---
+++ Day_33/Party.java
@@ -0,0 +1,92 @@
+/*
+    Junaeid As Jknight
+    Somoy paile dekha koiro
+    alap-adda-gopposob hobe
+    cha or coffee er sathe.
+------<<<<<<*>>>>>>>>-----
+D-33 Problem - 115A - Party
+*/
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+public class Party {
+    static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next(){
+            while(st==null || !st.hasMoreTokens()){
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+        long nextLong(){
+            return Long.parseLong(next());
+        }
+        double nextDouble(){
+            return Double.parseDouble(next());
+        }
+        String nextLine(){
+            String str="";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+    public static void main(String[] args){
+        try {
+            FastReader s = new FastReader();
+            FastWriter out = new FastWriter();
+            int ar[],n,i,boss,maxHier;
+            n = s.nextInt();    ar = new int[n+1];  i = 0;
+                while(i < n){
+                    boss = s.nextInt();
+                    if(boss != -1)ar[boss]++;
+                    ++i;
+                }   i = 2;  maxHier = ar[1];
+                while(i <= n){
+                    if(maxHier < ar[i])maxHier = ar[i];
+                    ++i;
+                }
+            out.println(maxHier+1);
+            out.close();
+        } catch (Exception e) {
+            return;
+        }
+    }
+}
