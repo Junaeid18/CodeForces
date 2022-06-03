3d4caeb94f4fbcc13817b80f49d648b382217246
Junaeid18 <mdjunaeid98@gmail.com>
2022-06-03 10:44:51 +0600

    Problem 686A Free Ice Cream Solved

---
+++ Day_33/FreeIceCream.java
@@ -0,0 +1,94 @@
+/*
+    Junaeid As Jknight
+    Somoy paile dekha koiro
+    alap-adda-gopposob hobe
+    cha or coffee er sathe.
+------<<<<<<*>>>>>>>>-----
+D-33 Problem - 686A - Free Ice Cream 
+*/
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+public class FreeIceCream {
+    static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next(){
+            while(st==null || !st.hasMoreTokens()){
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+        long nextLong(){
+            return Long.parseLong(next());
+        }
+        double nextDouble(){
+            return Double.parseDouble(next());
+        }
+        String nextLine(){
+            String str="";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+    public static void main(String[] args){
+        try {
+            FastReader s = new FastReader();
+            FastWriter out = new FastWriter();
+            int testCases = s.nextInt(),distress=0,size;
+            long x = s.nextLong(),di;
+            String input;   
+            while(testCases-- > 0){
+                input = s.nextLine();   size = input.length();
+                di = Long.parseLong(input.substring(2, size));
+                if(input.charAt(0) == '+'){
+                    x += di;
+                }else{
+                    if(x >= di)x -= di;
+                    else ++distress;
+                }
+            }
+            out.println(x+" "+distress);
+            out.close();
+        } catch (Exception e) {
+            return;
+        }
+    }
+}
