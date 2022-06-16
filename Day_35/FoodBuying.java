144fcf19578ff4ea7f0c7bc133e70d45dbc2150f
Junaeid18 <mdjunaeid98@gmail.com>
2022-06-16 17:17:07 +0600

    Problem 1296B Food Buying solved

---
+++ Day_35/FoodBuying.java
@@ -0,0 +1,95 @@
+/*
+    Junaeid As Jknight
+    Somoy paile dekha koiro
+    alap-adda-gopposob hobe
+    cha or coffee er sathe.
+------<<<<<<*>>>>>>>>-----
+D-34 Problem - Food Buying - 1296B
+*/
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import static java.lang.Math.round;
+public class FoodBuying {
+    static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next(){
+            while(st==null || !st.hasMoreTokens()){
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+        long nextLong(){
+            return Long.parseLong(next());
+        }
+        double nextDouble(){
+            return Double.parseDouble(next());
+        }
+        String nextLine(){
+            String str="";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+    public static void main(String[] args){
+        try {
+            FastReader s = new FastReader();
+            FastWriter out = new FastWriter();
+            int testCases = s.nextInt(),num,rem,totalUsed,currUse;
+            boolean status;
+            while(testCases-- > 0){
+                num = s.nextInt();  status = false; totalUsed = currUse = 0;
+                while(!status){
+                    rem = num % 10; currUse = num - rem;    totalUsed += currUse; 
+                    num = ((int)round((double) currUse/10)) + rem;
+                    if(num < 10){
+                        status = true;  totalUsed += num;
+                    }
+                }
+                out.println(totalUsed);  
+            }
+            out.close();
+        } catch (Exception e) {
+            return;
+        }
+    }
+}
+
