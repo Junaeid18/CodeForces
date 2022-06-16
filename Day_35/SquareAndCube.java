6c148941600495653ec834cc02f6d21251f51df4
Junaeid18 <mdjunaeid98@gmail.com>
2022-06-17 02:34:02 +0600

    Problem 1619B Square and Cubes solved

---
+++ Day_35/SquareAndCube.java
@@ -0,0 +1,89 @@
+/*
+    Junaeid As Jknight
+    Somoy paile dekha koiro
+    alap-adda-gopposob hobe
+    cha or coffee er sathe.
+------<<<<<<*>>>>>>>>-----
+D-35 Problem - Square And Cube - 1619B
+*/
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.HashSet;
+public class SquareAndCube {
+    static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next(){
+            while(st==null || !st.hasMoreTokens()){
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+        long nextLong(){
+            return Long.parseLong(next());
+        }
+        double nextDouble(){
+            return Double.parseDouble(next());
+        }
+        String nextLine(){
+            String str="";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+    public static void main(String[] args){
+        try {
+            FastReader s = new FastReader();
+            FastWriter out = new FastWriter();
+            HashSet<Integer> set = new HashSet<>();
+            int testCases = s.nextInt(),num;
+            while(testCases-- > 0){
+                num = s.nextInt();  set.clear();
+                for (int i = 1; (i*i) <= num; i++) { set.add((i*i)); }
+                for (int i = 1; (i*i*i) <= num; i++) {  set.add((i*i*i));   }
+                out.println(set.size());
+            }
+            out.close();
+        } catch (Exception e) {
+            return;
+        }
+    }
+}
