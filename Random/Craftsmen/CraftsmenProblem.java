50222e0ceb4717cae1cf4a23748348a9c891bfeb
Junaeid18 <mdjunaeid98@gmail.com>
2022-06-21 01:11:37 +0600

    Problem in craftsmen tried

---
+++ Random/Craftsmen/CraftsmenProblem.java
@@ -0,0 +1,95 @@
+/*
+    Md Junaeid Bhuiyan 
+------<<<<<<*>>>>>>>>-----
+*/
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import static java.lang.Math.min;
+public class CraftsmenProblem {
+    static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next(){
+            while(st==null || !st.hasMoreTokens()){
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+        long nextLong(){
+            return Long.parseLong(next());
+        }
+        double nextDouble(){
+            return Double.parseDouble(next());
+        }
+        String nextLine(){
+            String str="";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+    public static void main(String[] args){
+        try {
+            FastReader s = new FastReader();
+            FastWriter out = new FastWriter();
+            int length, i=0, maxL = 0, currentL = 0,currentR=0,maxR=0,result;
+            String input = s.nextLine();    length = input.length();
+            char c;
+            while(i < length){
+                c = input.charAt(i);
+                if(c == '<'){
+                    ++currentL;  
+                    maxL = (currentL > maxL)? currentL : maxL;
+                    currentR = (currentR > 0)? --currentR : currentR;
+                }else if(c == '>'){
+                    currentL = (currentL > 0)? --currentL : currentL;
+                    ++currentR;
+                    maxR = (currentR > maxR)?currentR : maxR;
+                }
+                ++i;
+            }
+            result = min(maxL,maxR);
+            out.println(result);
+            out.close();
+        } catch (Exception e) {
+            return;
+        }
+    }
+}
