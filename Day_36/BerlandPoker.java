77d29bacdd76337b80a65b3657dfebd31b6c2958
Junaeid18 <mdjunaeid98@gmail.com>
2022-07-03 23:24:40 +0600

    Problem 1359A Berland Poker solved

---
+++ Day_36/BerlandPoker.java
@@ -0,0 +1,96 @@
+/*
+    Junaeid As Jknight
+    Somoy paile dekha koiro
+    alap-adda-gopposob hobe
+    cha or coffee er sathe.
+------<<<<<<*>>>>>>>>-----
+D-36 Problem - Berland Poker - 1359A
+*/
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+public class BerlandPoker {
+    static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next(){
+            while(st==null || !st.hasMoreTokens()){
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+        long nextLong(){
+            return Long.parseLong(next());
+        }
+        double nextDouble(){
+            return Double.parseDouble(next());
+        }
+        String nextLine(){
+            String str="";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+    public static void main(String[] args){
+        try {
+            FastReader s = new FastReader();
+            FastWriter out = new FastWriter();
+            int testCases = s.nextInt(),n,m,k,rem,eachPlayer,result,i;
+            while(testCases-- > 0){
+                n = s.nextInt();    m = s.nextInt();    k = s.nextInt();
+                if(m == 0) result = 0;
+                else{
+                    eachPlayer = n / k; 
+                    if(m >= eachPlayer){
+                        rem = m - eachPlayer;
+                        i = 0; 
+                        while(rem > ((k-1)* i)) ++i;
+                        result = eachPlayer - i;
+                    }
+                    else result = m;
+                }
+                out.println(result); 
+            }
+            out.close();
+        } catch (Exception e) {
+            return;
+        }
+    }
+}
