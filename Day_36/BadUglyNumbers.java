d484123b6525d33beab690d9fb24b5e1f9fccc9e
Junaeid18 <mdjunaeid98@gmail.com>
2022-07-13 00:36:09 +0600

    Problem 1326A Bad Ugly Numbers solved

---
+++ Day_36/BadUglyNumbers.java
@@ -0,0 +1,93 @@
+/*
+    Junaeid As Jknight
+    Somoy paile dekha koiro
+    alap-adda-gopposob hobe
+    cha or coffee er sathe.
+------<<<<<<*>>>>>>>>-----
+D-36 Problem -  1326A - Bad Ugly Numbers
+*/
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+public class BadUglyNumbers {
+    static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next(){
+            while(st==null || !st.hasMoreTokens()){
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+        long nextLong(){
+            return Long.parseLong(next());
+        }
+        double nextDouble(){
+            return Double.parseDouble(next());
+        }
+        String nextLine(){
+            String str="";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+    public static void main(String[] args){
+        try {
+            FastReader s = new FastReader();
+            FastWriter out = new FastWriter();
+            int testCases = s.nextInt(),n;
+            StringBuilder output = new StringBuilder();
+            while(testCases-- > 0) {
+                n = s.nextInt();
+                if(n >= 2){
+                    output.append(2);
+                    for (int i = 1; i < n; i++) {
+                        output.append(3);
+                    }
+                    out.println(output);
+                }else out.println(-1);
+                output.setLength(0);
+            }
+            out.close();
+        } catch (Exception e) {
+            return;
+        }
+    }
+}
