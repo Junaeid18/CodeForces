d288beac53ea94fb619f61c0da6a932938503826
Junaeid18 <mdjunaeid98@gmail.com>
2022-07-23 22:07:26 +0600

    Problem 1652A ABC solved

---
+++ Day_37/ABC.java
@@ -0,0 +1,98 @@
+/*
+    Junaeid As Jknight
+    Somoy paile dekha koiro
+    alap-adda-gopposob hobe
+    cha or coffee er sathe.
+------<<<<<<*>>>>>>>>-----
+D-37 Problem - ABC - 1632 - A
+*/
+import java.util.StringTokenizer;
+
+import javax.swing.text.DefaultStyledDocument.ElementSpec;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+public class ABC {
+    static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next(){
+            while(st==null || !st.hasMoreTokens()){
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+        long nextLong(){
+            return Long.parseLong(next());
+        }
+        double nextDouble(){
+            return Double.parseDouble(next());
+        }
+        String nextLine(){
+            String str="";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+    public static void main(String[] args){
+        try {
+            FastReader s = new FastReader();
+            FastWriter out = new FastWriter();
+            int testCases = s.nextInt(),length;
+            String input;
+            while(testCases-- > 0) {
+                length = s.nextInt();   input = s.nextLine();
+                if(length >= 3)out.println("NO");
+                else{
+                    if(length == 1)out.println("YES");
+                    else{
+                        if(input.charAt(0) == input.charAt(1))out.println("NO");
+                        else out.println("YES");
+                    }
+                }
+            }
+
+
+            out.close();
+        } catch (Exception e) {
+            return;
+        }
+    }
+}
